0ef54b2e31255da68f8192e16295466f71a6454d
bhavsarmohit <mohitamalner@gmail.com>
2021-09-19 18:55:00 +0530

    improvements
    add subjects in nav bar,
    code improvement,
    bug fixes menu dashboard

--- js/dashboard-nav.js
+++ js/dashboard-nav.js
@@ -1,6 +1,13 @@
 function show(shown) {
   hide_pages();
+
+  console.log(shown);
+  nav_dash_manage(shown);
+  
   document.getElementById(shown).style.display='block';
+
+
+
   return false;
 }
 
@@ -10,6 +17,10 @@ function hide_pages(shown) {
   document.getElementById('divisions-view').style.display='none';
   document.getElementById('divisions-add').style.display='none';
   document.getElementById('divisions-modify').style.display='none';
+
+  document.getElementById('subjects-view').style.display='none';
+  document.getElementById('subjects-add').style.display='none';
+  document.getElementById('subjects-modify').style.display='none';
 
   document.getElementById('class-view').style.display='none';
   document.getElementById('class-add').style.display='none';
@@ -30,4 +41,34 @@ function hide_pages(shown) {
   document.getElementById('analysis').style.display='none';
   document.getElementById('reports').style.display='none';
   return false;
+}
+
+function set_nav_component_active(id){
+  const el = document.getElementById(id);
+
+  el.className = el.className + " active";
+  
+  console.log("classname active:"+el.className);
+}
+
+function set_nav_component_unactive(id){
+  const el = document.getElementById(id);
+
+  var classnm = el.className;
+  
+  classnm = classnm.replace(' active','');
+  el.className = classnm
+
+}
+
+
+function nav_dash_manage(id){
+  
+  set_nav_component_unactive("nav_dashboard");
+  if(id=="dashboard"){
+    set_nav_component_active("nav_dashboard")
+  }
+  // else{
+  //   set_nav_component_unactive("nav_dash");
+  // }
 }
